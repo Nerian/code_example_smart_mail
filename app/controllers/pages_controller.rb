9cc93de001594d8a26db3901ba9dfacfeebc8051
Nerian <siotopo@gmail.com>
2019-02-12 13:19:40 +0100

    Mockup for what I'd like to build

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
 
   def index
-
+    @mock_data = [
+      { name: "BRL", data: { 1.days.ago => 100, 2.days.ago => 110, 3.days.ago => 120 } },
+      { name: "EUR", data: { 1.days.ago => 200, 2.days.ago => 210, 3.days.ago => 220 } },
+      { name: "AUD", data: { 1.days.ago => 340, 2.days.ago => 360, 3.days.ago => 390 } },
+    ]
   end
 end
