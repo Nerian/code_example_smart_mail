a0ef59ebe493691d36f0b2fbd48e22a3ea133ec6
Nerian <siotopo@gmail.com>
2019-02-12 17:06:31 +0100

    Refactor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,28 +5,6 @@ class PagesController < ApplicationController
     @origin            = params[:origin]
     @target_currencies = (@currencies - [@origin])
 
-    @data = []
-
-    @target_currencies.each do |currency|
-      @data << { name: currency, data: get_data(currency) }
-    end
-  end
-
-  private
-
-  def get_data(currency)
-    data = {}
-
-    (600.days.ago.to_date..1.days.ago.to_date).each do |date|
-      begin
-        data[date]       = Money.default_bank.exchange(100, @origin, currency, date).to_s
-        @last_valid_rate = data[date]
-      rescue Money::Bank::UnknownRate
-        # eu_bank does not publish exchange rates on weekends. So we infer the value.
-        data[date] = @last_valid_rate
-      end
-    end
-
-    data
+    @data = CurrencyReporter.new(origin: @origin, targets: @target_currencies).report
   end
 end
---
+++ app/poro/currency_reporter.rb
@@ -0,0 +1,40 @@
+class CurrencyReporter
+
+  attr_accessor :origin, :targets, :data
+
+  def initialize(origin:, targets:, days: 600)
+    @origin  = origin
+    @targets = targets
+    @data    = []
+    @days    = 600
+  end
+
+  def report
+    @data = []
+
+    @targets.each do |currency|
+      @data << { name: currency, data: get_data(currency) }
+    end
+
+    @data
+  end
+
+  private
+
+  def get_data(currency)
+    currency_data = {}
+
+    (@days.days.ago.to_date..1.days.ago.to_date).each do |date|
+      begin
+        currency_data[date] = Money.default_bank.exchange(100, @origin, currency, date).to_s
+        @last_valid_rate    = currency_data[date]
+      rescue Money::Bank::UnknownRate
+        # eu_bank does not publish exchange rates on weekends. So we take the previous value.
+        currency_data[date] = @last_valid_rate
+      end
+    end
+
+    currency_data
+  end
+
+end
